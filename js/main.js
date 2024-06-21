0413f9e5d6d4e5ff6cdb9a555ee18dc5c6c56877
jasonpopejoy <51867066+jasonpopejoy@users.noreply.github.com>
2024-06-20 18:15:13 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('a.blog-button').click(function (e) {
+/*  $('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
@@ -10,7 +10,7 @@ $(document).ready(function () {
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
-
+*/
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
