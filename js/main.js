76cee7827fd5a4c355e1e80fa75c52c194177c88
jasonpopejoy <51867066+jasonpopejoy@users.noreply.github.com>
2024-06-20 18:22:50 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,11 +14,11 @@ $(document).ready(function () {
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
-*/
+
   if (window.location.pathname !== '/' && window.location.pathname !== '/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
-
+*/
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
